e69dc0e81159dcdcd1fb06173e578e0fe32b8d4b
Rinki Bera <rinkibera2211@gmail.com>
2026-05-26 11:42:06 +0530

    Function

---
+++ ArrSum.js
@@ -0,0 +1,9 @@
+function ArrSum(a) {
+  let sum = 0;
+  for (let i = 0; i < a.length; i++) {
+    sum += a[i];
+  }
+  console.log(sum);
+}
+ArrSum([1, 2, 3]);
+ArrSum([3, 7, 5]);
---
+++ array.js
@@ -0,0 +1,2 @@
+const students = ["Rinki", "Anu", 36, ["Rim", 56, "True"], "True"];
+console.log(students[1], students[3][2]);
